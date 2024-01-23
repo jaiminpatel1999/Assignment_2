49d3480c65f987b6df3351adc735b6bfc9182123
jaiminpatel1999 <jdp9662@gmail.com>
2024-01-23 16:52:09 -0500

    Initial Commit

---
+++ Assignment_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_2
+//
+//  Created by user237118 on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
